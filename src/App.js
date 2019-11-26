2b8a16303c7e6e17a079d782131771f0013a587a
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-11-26 16:48:38 +0900

    15

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 const App = () => {
-  // return <div>HERE</div>
-  // returnとしてjsxを変えしている
+  const profiles = [
+    {name: "taro",age: "10" },
+    {name: "Hanako",age: "5" }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+    {
+      profiles.map((profile) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })
+    }
     </div>
+    // divないの全てのものが表示されている
+    // {}での書き方はjsでの書き方
+    // / を入れることによって管理されている
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
-  // 上のcatにmeow!を代入しているイメージ
+const User = (props) => {
+  return <div>MY name is {props.name}, and {props.age} years old</div>
+// 名前を表示したいところに上記で使用したpropsを代入してあげることによって表示される
 }
+
+User.defaultProps = {
+  age: 1
+}
+// 何もないときに代わりに表示してくれる
 export default App;
