efe46ed384db37fd886964ef9d5f3ab70287406f
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-11-26 12:59:25 +0900

    14

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-// reactをimportしなければJSXを使うことができない
-function App() {
-  // const greeting = "onigiri";
-  // const dom = <h1 className>Hello wprld or {greeting}</h1>; ここの行でdomという変数にhello worldを定義している(greetingも出力可能)
-  // return dom;  domという変数を表示している
 
-//  return <input type="text" onClick={() => {console.log("you click find day!")}} />;
-  // テキストタイプでクリックされたらyou click find dayを表示するというもの
+const App = () => {
+  // return <div>HERE</div>
+  // returnとしてjsxを変えしている
   return (
-    <React.Fragment>
-    <label htmlFor="bar">bar</label>
-    <input type="text" onChange={() => {console.log("I love you")}} />
-    </React.Fragment>
-    // 一つのタグでならないといけない ..htmlfor labelに付与することで、同じ内容のid属性を持つ要素と関連付けられる
-    // react.fragmentを使用することによってdivタグを使わなくても済む
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+  // 上のcatにmeow!を代入しているイメージ
+}
 export default App;
