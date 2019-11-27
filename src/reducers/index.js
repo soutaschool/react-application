dd68dd66f4e37831c4271f3158adef85fcede777
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-11-27 22:45:22 +0900

    setting-before

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 // すべてのレデューサーを結合する役割
 import { combineReducers } from 'redux'
 // combineReducersが結合する役割の関数
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
 // countの部分は複数のものを書くことも可能
